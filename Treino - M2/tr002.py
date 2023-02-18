a5207751ff95e666b79d48bc24448f8c5bb41c47
Thales Gregório <Thales-henrry@hotmail.com>
2023-02-18 02:03:27 -0300

    Inconsistente

--- Treino - M2/tr002.py
+++ Treino - M2/tr002.py
@@ -1,8 +1,11 @@
-#26 Numa eleição existem três candidatos. Faça um programa que peça o número total de eleitores.
+# 26 Numa eleição existem três candidatos. Faça um programa que peça o número total de eleitores.
 # Peça para cada eleitor votar e ao final mostrar o número de votos de cada candidato.
-g=0
-s=0
-f=0
+eleitor1 = 'Gabiru da Silva'
+g = 0
+eleitor2 = 'Selena Gomes'
+s = 0
+eleitor3 = 'Faustão'
+f = 0
 
 eleitores = int(input('Quantidade de eleitores: '))
 
@@ -21,34 +24,59 @@ for c in range(0, eleitores):
     else:
         print('Voto Incorreto')
 
+    # print(f'G:{g}\n'
+    #       f'S:{s}\n'
+    #       f'F:{f}')
+
 print('=======RESULTADO=======')
 
-#CAMPEÃO
+# CAMPEÃO
 camp = g
 if g > s and f:
-    camp = g
+    camp = eleitor1
 elif s > g and f:
-    camp = s
+    camp = eleitor2
 else:
-    camp = f
+    camp = eleitor3
 
-#ULTIMO
+# ULTIMO
 ult = g
 if g < s and f:
-    ult = g
+    ult = eleitor1
 elif s < g and f:
-    ult = s
+    ult = eleitor2
 else:
-    ult = f
+    ult = eleitor3
 
-#VICE
+# VICE
 vice = g
-if camp == g or camp == s:
-    vice = f
-elif camp == s or camp == f:
-    vice = g
+if camp == eleitor1 and ult == eleitor3:
+    vice = eleitor2
+elif camp == eleitor2 and ult == eleitor3:
+    vice = eleitor1
 else:
-    vice = s
+    vice = eleitor3
 
-print(f'CAMPEÃO: {camp} ')
+# RESULTADO CAMPEÃO
+if camp == eleitor1:
+    print(f'CAMPEÃO: {camp} | VOTOS {g} ')
+elif camp == eleitor2:
+    print(f'CAMPEÃO: {camp} | VOTOS {s} ')
+else:
+    print(f'CAMPEÃO: {camp} | VOTOS {f} ')
 
+# RESULTADO VICE
+if vice == eleitor1:
+    print(f'VICE: {vice} | VOTOS {g} ')
+elif vice == eleitor2:
+    print(f'VICE: {vice} | VOTOS {s} ')
+else:
+    print(f'VICE: {vice} | VOTOS {f} ')
+
+# RESULTADO ULTIMO
+if ult == eleitor1:
+    print(f'ULTIMO: {ult} | VOTOS {g} ')
+elif ult == eleitor2:
+    print(f'ULTIMO: {ult} | VOTOS {s} ')
+else:
+    print(f'ULTIMO: {ult} | VOTOS {f} ')
