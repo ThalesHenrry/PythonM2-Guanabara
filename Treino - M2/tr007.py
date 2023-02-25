1b199a4aa889e9782f9f8c54b028611727a7a583
Thales Gregório <Thales-henrry@hotmail.com>
2023-02-25 00:08:21 -0300

    Treino 7 | Funcional

--- Treino - M2/tr007.py
+++ Treino - M2/tr007.py
@@ -1,8 +1,9 @@
 # Faça um programa que calcule o valor total investido por um colecionador em sua coleção de CDs e o valor médio gasto em cada um deles.
 # O usuário deverá informar a quantidade de CDs e o valor para em cada um.
+
 p = 1
 c = 0
-soma=0
+soma = 0
 lista_cd = []
 quantidade_cd = int(input('Quantidade de CD: '))
 
@@ -15,6 +16,5 @@ while quantidade_cd > c:
 for lista_cd in lista_cd:
     soma += lista_cd
 
-
-print(f'E a média gasta por Cd é de R${soma / quantidade_cd}')
-print(f' A soma total de gastos com Cds é R${soma:.2f}')
+print(f'A média gasta por Cd é de R${soma/(p-1):.2f}')
+print(f'E a soma total de gastos com Cds é R${soma:.2f}')
